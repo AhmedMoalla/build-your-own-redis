74d3f9d4f13ea71039b6eab02fc73eb020acbec4
Ahmed Moalla <ahmed.moalla@novencia.com>
2024-03-17 01:40:50 +0100

    Stage 1/7: Bind to a port

--- src/main/java/com/amoalla/redis/Main.java
+++ src/main/java/com/amoalla/redis/Main.java
@@ -1,5 +1,33 @@
+package com.amoalla.redis;
+
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
-  public static void main(String[] args){
-    
-  }
+
+    private static final int PORT = 6379;
+
+    public static void main(String[] args) throws IOException {
+        IoAcceptor acceptor = new NioSocketAcceptor();
+
+        acceptor.getFilterChain().addLast("logger", new LoggingFilter());
+        acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new TextLineCodecFactory(StandardCharsets.UTF_8)));
+        acceptor.setHandler(new RedisHandler());
+        acceptor.getSessionConfig().setReadBufferSize(2048);
+        acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);
+        acceptor.bind(new InetSocketAddress(PORT));
+    }
+}
+
+class RedisHandler extends IoHandlerAdapter {
+
 }
