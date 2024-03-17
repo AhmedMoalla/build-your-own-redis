8392aa44dc9b7c5db9d8a6a5ba9c47d706178dac
Ahmed Moalla <ahmed.moalla@novencia.com>
2024-03-17 01:45:34 +0100

    Stage 1/7: Close session on connect

--- src/main/java/com/amoalla/redis/Main.java
+++ src/main/java/com/amoalla/redis/Main.java
@@ -3,9 +3,11 @@ package com.amoalla.redis;
 import org.apache.mina.core.service.IoAcceptor;
 import org.apache.mina.core.service.IoHandlerAdapter;
 import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
 import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.SocketAcceptor;
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 
 import java.io.IOException;
@@ -17,17 +19,39 @@ public class Main {
     private static final int PORT = 6379;
 
     public static void main(String[] args) throws IOException {
-        IoAcceptor acceptor = new NioSocketAcceptor();
-
+        SocketAcceptor acceptor = new NioSocketAcceptor();
         acceptor.getFilterChain().addLast("logger", new LoggingFilter());
         acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new TextLineCodecFactory(StandardCharsets.UTF_8)));
         acceptor.setHandler(new RedisHandler());
         acceptor.getSessionConfig().setReadBufferSize(2048);
         acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);
+        acceptor.setReuseAddress(true);
         acceptor.bind(new InetSocketAddress(PORT));
     }
 }
 
 class RedisHandler extends IoHandlerAdapter {
 
+    public void sessionOpened(IoSession session) throws Exception {
+        System.out.println("Session opened: " + session.getId());
+        session.closeNow();
+        System.exit(0);
+    }
+
+    @Override
+    public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
+        cause.printStackTrace();
+    }
+
+    @Override
+    public void messageReceived(IoSession session, Object message) throws Exception {
+        String str = message.toString();
+        System.out.println("Message Received: " + str);
+        session.closeNow();
+    }
+
+    @Override
+    public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
+        System.out.println("IDLE " + session.getIdleCount(status));
+    }
 }
